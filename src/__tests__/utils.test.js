3bc77428de4baba3b0b25120676ee3ce0a2da189
Arthur Ish <arthurishdev@gmail.com>
2025-01-21 15:21:50 -0800

    test passing

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -8,4 +8,15 @@ describe("pointsForWord", () => {
 
     expect(points).toBe(7);
   });
+
+
+it("handles uppercase and lowercase input", () => {
+    const word = "tEsT";
+  
+    const points = pointsForWord(word);
+  
+    expect(points).toBe(7);
+  });
+  
+  
 });
